5b2699f1f2db0ffb806798109c89ec8f8691a18a
XavierBadayos <xavierbadayos@gmail.com>
2026-04-08 01:54:22 +0930

    add PlayerCard interface

---
+++ src/types/PlayerCard.ts
@@ -0,0 +1,20 @@
+export interface PlayerCard {
+  id: string | number;
+  name: string | number;
+  nickname: string | number;
+  teamId: string | number;
+  teamAbbreviation: string | number;
+  gp: string | number;
+  w: string | number;
+  l: string | number;
+  min: string | number;
+  pts: string | number;
+  ast: string | number;
+  reb: string | number;
+  fga: string | number;
+  fgm: string | number;
+  fgPct: string | number;
+  fta: string | number;
+  ftm: string | number;
+  ftPct: string | number;
+}

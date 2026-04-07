1db3a8c07046408c3d275f6da1f840f93f24f9eb
XavierBadayos <xavierbadayos@gmail.com>
2026-04-08 04:40:27 +0930

    add data to PlayerCard

--- src/components/players/PlayerCard.tsx
+++ src/components/players/PlayerCard.tsx
@@ -1,5 +1,6 @@
 import type { PlayerCardData } from "@/types/PlayerCard"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card"
+import { Card, CardAction, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card"
+import { Badge } from "../ui/badge"
 
 interface PlayerCardProps {
   data: PlayerCardData 
@@ -8,16 +9,30 @@ interface PlayerCardProps {
 export const PlayerCard = ({data}: PlayerCardProps) => {
   return (
     <Card>
-      <img className="object-fit bg-slate-900" src={`https://cdn.nba.com/headshots/nba/latest/260x190/${data.id}.png`} alt={data.nickname as string}/>
-      <CardHeader>
-        <CardTitle>
-          <h3>{data.name}</h3>
-        </CardTitle>
-        <CardDescription></CardDescription>
+      <img className="object-fit bg-slate-800" src={data.playerImage} alt={data.nickname as string}/>
+      <CardHeader >
+          <div className="flex justify-between pb-2">
+            <CardTitle><h3>{data.name}</h3></CardTitle>
+            <Badge className="text-xs">{data.teamAbbreviation}</Badge>
+          </div>
+        <CardDescription className="flex justify-between">
+          <p>{data.pts} pts</p>
+          <p>{data.reb} reb</p>
+          <p>{data.ast} ast</p>
+        </CardDescription>
       </CardHeader>
-      <CardContent>
-        <p></p>
+      <CardContent className="grid grid-cols-2">
+        <div>
+          <p>{data.min} MIN</p>
+          <p>{data.fga} FGA</p>
+          <p>{(data.fgPct as number * 100).toFixed(1)} FG%</p>
+        </div>
+        <div>
+          <p>{(data.fga as number).toFixed(1)} FTA</p>
+          <p>{(data.fta as number).toFixed(1)} FT%</p>
+          <p>{data.w}-{data.l} W-L</p>
+        </div>
       </CardContent>
-    </Card>
+    </Card> 
   )
 }
--- src/components/players/PlayerCardList.tsx
+++ src/components/players/PlayerCardList.tsx
@@ -7,7 +7,7 @@ interface PlayerCardListProps {
 
 export const PlayerCardList = ({data}: PlayerCardListProps) => {
     return (
-        <div className="grid grid-cols-5 gap-3">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3">
             {data.map(player => 
                 <PlayerCard key={player.id} data={player}/>
             )}

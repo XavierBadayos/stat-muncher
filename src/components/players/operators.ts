02f444259602cda62abc48df8e13d6e73fe1b589
XavierBadayos <xavierbadayos@gmail.com>
2026-04-12 01:18:23 +0930

    add operators and conditions array map

---
+++ src/components/players/operators.ts
@@ -0,0 +1,11 @@
+export const operatorsArray = [
+    {label: "=", value: "="},
+    {label: ">", value: ">"},
+    {label: ">=", value: ">="},
+    {label: "<", value: "<"},
+    {label: "<=", value: "<="}
+];
+export const conditionsArray = [
+    {label: "AND", value: "AND"},
+    {label: "OR", value: "OR"}
+];
